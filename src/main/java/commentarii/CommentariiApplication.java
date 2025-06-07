30b5fa8995171468e9923f3a6aec280168f7998a
raghav309 <raghav03ef@gmail.com>
2025-06-07 15:30:01 +0530

    Initial project setup

---
+++ src/main/java/commentarii/CommentariiApplication.java
@@ -0,0 +1,13 @@
+package commentarii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentariiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentariiApplication.class, args);
+	}
+
+}
